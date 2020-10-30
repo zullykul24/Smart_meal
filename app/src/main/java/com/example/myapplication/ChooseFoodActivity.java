3cb7d0938c6f87e7debb95ada378cec4446ce203
Nguyen Phuc Khoi <zullykulphinh@gmail.com>
2020-10-31 00:35:29 +0700

    lỗi scrollview

--- app/src/main/java/com/example/myapplication/ChooseFoodActivity.java
+++ app/src/main/java/com/example/myapplication/ChooseFoodActivity.java
@@ -25,18 +25,18 @@ public class ChooseFoodActivity extends AppCompatActivity {
 
         listViewFood = (ListView)findViewById(R.id.listViewFoodMenu);
         menuItemArrayList = new ArrayList<>();
-        menuItemArrayList.add(new MenuFoodItem("Bò sốt tiêu đen + Bánh bao",20.000, R.drawable.mango));
-        menuItemArrayList.add(new MenuFoodItem("Bò sốt tiêu đen + Bánh bao",20.000, R.drawable.mango));
-        menuItemArrayList.add(new MenuFoodItem("Bò sốt tiêu đen + Bánh bao",20.000, R.drawable.mango));
-        menuItemArrayList.add(new MenuFoodItem("Bò sốt tiêu đen + Bánh bao",20.000, R.drawable.mango));
-        menuItemArrayList.add(new MenuFoodItem("Bò sốt tiêu đen + Bánh bao",20.000, R.drawable.mango));
-        menuItemArrayList.add(new MenuFoodItem("Bò sốt tiêu đen + Bánh bao",20.000, R.drawable.mango));
-        menuItemArrayList.add(new MenuFoodItem("Bò sốt tiêu đen + Bánh bao",20.000, R.drawable.mango));
-        menuItemArrayList.add(new MenuFoodItem("Bò sốt tiêu đen + Bánh bao",20.000, R.drawable.mango));
-        menuItemArrayList.add(new MenuFoodItem("Bò sốt tiêu đen + Bánh bao",20.000, R.drawable.mango));
-        menuItemArrayList.add(new MenuFoodItem("Bò sốt tiêu đen + Bánh bao",20.000, R.drawable.mango));
-        menuItemArrayList.add(new MenuFoodItem("Bò sốt tiêu đen + Bánh bao",20.000, R.drawable.mango));
-        menuItemArrayList.add(new MenuFoodItem("Bò sốt tiêu đen + Bánh bao",20.000, R.drawable.mango));
+        menuItemArrayList.add(new MenuFoodItem("Món 1",20.000, R.drawable.mango));
+        menuItemArrayList.add(new MenuFoodItem("Món 2",20.000, R.drawable.mango));
+        menuItemArrayList.add(new MenuFoodItem("Món 3",20.000, R.drawable.mango));
+        menuItemArrayList.add(new MenuFoodItem("Món 4",20.000, R.drawable.mango));
+        menuItemArrayList.add(new MenuFoodItem("Món 5",20.000, R.drawable.mango));
+        menuItemArrayList.add(new MenuFoodItem("Món 6",20.000, R.drawable.mango));
+        menuItemArrayList.add(new MenuFoodItem("Món 7",20.000, R.drawable.mango));
+        menuItemArrayList.add(new MenuFoodItem("Món 8",20.000, R.drawable.mango));
+        menuItemArrayList.add(new MenuFoodItem("Món 9",20.000, R.drawable.mango));
+        menuItemArrayList.add(new MenuFoodItem("Món 10",20.000, R.drawable.mango));
+        menuItemArrayList.add(new MenuFoodItem("Món 11",20.000, R.drawable.mango));
+        menuItemArrayList.add(new MenuFoodItem("Món 12",20.000, R.drawable.mango));
         menuFoodItemAdapter = new MenuFoodItemAdapter(ChooseFoodActivity.this, R.layout.menu_food_item, menuItemArrayList);
         listViewFood.setAdapter(menuFoodItemAdapter);
 
@@ -44,8 +44,8 @@ public class ChooseFoodActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent intentSendFoodToOrderActivity = new Intent(ChooseFoodActivity.this, OrderActivity.class);
-              //  MenuFoodItem item = new MenuFoodItem(menuItemArrayList.get(position).getDish_name(), menuItemArrayList.get(position).getPrice(), menuItemArrayList.get(position).getHinhAnh());
-               MenuFoodItem item = new MenuFoodItem("Bò sốt tiêu đen + Bánh bao",20.000, R.drawable.mango);
+                MenuFoodItem item = new MenuFoodItem(menuItemArrayList.get(position).getDish_name(), menuItemArrayList.get(position).getPrice(), menuItemArrayList.get(position).getHinhAnh());
+              // MenuFoodItem item = new MenuFoodItem("Bò sốt tiêu đen + Bánh bao",20.000, R.drawable.mango);
                 Toast.makeText(ChooseFoodActivity.this, item.getDish_name()+""+item.getPrice().toString(),Toast.LENGTH_SHORT).show();
                 intentSendFoodToOrderActivity.putExtra("abc", (Serializable) item);
                 setResult(Activity.RESULT_OK, intentSendFoodToOrderActivity);
