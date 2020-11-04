8e17e91147cea05f5b3f6e709f516fc2c9d6182b
Nguyen Phuc Khoi <zullykulphinh@gmail.com>
2020-11-04 21:30:52 +0700

    có thể trượt để xoá món đã chọn

--- app/src/main/java/com/example/myapplication/OrderActivity.java
+++ app/src/main/java/com/example/myapplication/OrderActivity.java
@@ -1,15 +1,24 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.baoyz.swipemenulistview.SwipeMenu;
+import com.baoyz.swipemenulistview.SwipeMenuCreator;
+import com.baoyz.swipemenulistview.SwipeMenuItem;
+import com.baoyz.swipemenulistview.SwipeMenuListView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,10 +26,10 @@ import java.util.List;
 public class OrderActivity extends AppCompatActivity {
     TextView tableName;
     Button themMonBtn;
-    ListView listViewChosenFood;
-    //ArrayList<MenuFoodItem> arrayListChosenFood;
+    SwipeMenuListView listViewChosenFood;
+
     ArrayList<FoodOrderItem> arrayListChosenFood;
-  //  MenuFoodItemAdapter adapterChosenFood;
+
     FoodOrderItemAdapter adapterChosenFood;
 
 
@@ -35,7 +44,7 @@ public class OrderActivity extends AppCompatActivity {
 
         tableName = (TextView)findViewById(R.id.nameOfIntentedTable);
         themMonBtn = (Button)findViewById(R.id.themMonBtn);
-        listViewChosenFood = (ListView)findViewById(R.id.listViewChosenFood);
+        listViewChosenFood = (SwipeMenuListView) findViewById(R.id.listViewChosenFood);
         View footer = getLayoutInflater().inflate(R.layout.footer, null);
         listViewChosenFood.addFooterView(footer);
 
@@ -65,6 +74,47 @@ public class OrderActivity extends AppCompatActivity {
                 arrayListChosenFood.add(new FoodOrderItem(foodItem.getDish_name(), foodItem.getPrice(), foodItem.getHinhAnh()));
                 adapterChosenFood = new FoodOrderItemAdapter(OrderActivity.this, R.layout.food_order_item, arrayListChosenFood);
                 listViewChosenFood.setAdapter(adapterChosenFood);
+                // Thêm trượt để xoá https://github.com/baoyongzhang/SwipeMenuListView
+                SwipeMenuCreator creator = new SwipeMenuCreator() {
+
+                    @Override
+                    public void create(SwipeMenu menu) {
+                        // create "delete" item
+                        SwipeMenuItem deleteItem = new SwipeMenuItem(
+                                getApplicationContext());
+                        // set item background
+                        deleteItem.setBackground(new ColorDrawable(Color.rgb(0xff, 0x6e,
+                                0x2d)));
+                        // set item width
+                        deleteItem.setWidth(170);
+                        // set item title
+                        deleteItem.setTitle("Xoá");
+                        // set item title fontsize
+                        deleteItem.setTitleSize(18);
+                        // set item title font color
+                        deleteItem.setTitleColor(Color.WHITE);
+                        // add to menu
+                        menu.addMenuItem(deleteItem);
+
+
+                        //có thể setIcon
+
+
+                    }
+                };
+                listViewChosenFood.setMenuCreator(creator);
+                /// set click cho item ở menu trượt
+                listViewChosenFood.setOnMenuItemClickListener(new SwipeMenuListView.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(int position, SwipeMenu menu, int index) {
+                        Toast.makeText(getApplicationContext(),arrayListChosenFood.get(position).getDish_name(),Toast.LENGTH_SHORT).show();
+                       arrayListChosenFood.remove(position);
+                       adapterChosenFood.notifyDataSetChanged();
+                        // false : close the menu; true : not close the menu
+                        return false;
+                    }
+                });
+
             }
         }
     }
